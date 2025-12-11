2298cae03b3ddacd2200ac563f2d96647f070b2b
Hyziri <hyziri@protonmail.com>
2025-12-11 00:49:33 -0600

    refactor: redirect user from login page if already logged in

--- src/client/route/login.rs
+++ src/client/route/login.rs
@@ -1,9 +1,22 @@
 use dioxus::prelude::*;
 
-use crate::client::component::Page;
+use crate::client::{component::Page, router::Route, store::user::UserState};
 
 #[component]
 pub fn Login() -> Element {
+    let user_store = use_context::<Store<UserState>>();
+    let nav = navigator();
+
+    let user_logged_in = user_store.read().user.is_some();
+    let fetch_completed = user_store.read().fetched;
+
+    // Redirect authenticed user to home after fetch completes
+    use_effect(use_reactive!(|(user_logged_in, fetch_completed)| {
+        if user_logged_in && fetch_completed {
+            nav.push(Route::Home {});
+        }
+    }));
+
     rsx! {
         Title { "Login | Black Rose Timerboard" }
         Page {
