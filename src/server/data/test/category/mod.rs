759affb87d36efcfbc6f0a10e30d1c2e985bb382
Hyziri <hyziri@protonmail.com>
2025-12-20 02:14:08 -0600

    tests: add missing category repository unit tests

--- src/server/data/test/category/mod.rs
+++ src/server/data/test/category/mod.rs
@@ -8,6 +8,12 @@ use test_utils::{builder::TestBuilder, factory};
 
 mod create;
 mod delete;
+mod exists_in_guild;
 mod get_by_guild_id_paginated;
 mod get_by_id;
+mod get_by_ping_format_id;
+mod get_category_details;
 mod update;
+mod user_can_create_category;
+mod user_can_manage_category;
+mod user_can_view_category;
