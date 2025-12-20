eeff46605e48f6bc5391720a5fd365b71d804740
Hyziri <hyziri@protonmail.com>
2025-12-20 02:02:02 -0600

    refactor: add OG image & site description metadata

--- src/client/app.rs
+++ src/client/app.rs
@@ -5,6 +5,13 @@ use crate::client::{constant::SITE_NAME, router::Route, store::user::UserState};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+const LOGO: Asset = asset!(
+    "/assets/logo.webp",
+    AssetOptions::image().with_size(ImageSize::Manual {
+        width: 256,
+        height: 256
+    })
+);
 
 #[component]
 pub fn App() -> Element {
@@ -41,6 +48,18 @@ pub fn App() -> Element {
     rsx! {
         Title { "{SITE_NAME}" }
         document::Link { rel: "icon", href: FAVICON }
+        document::Meta {
+            name: "og:image",
+            content: LOGO
+        }
+        document::Meta {
+            name: "twitter:image",
+            content: LOGO
+        }
+        document::Meta {
+            name: "description",
+            content: " Discord-based fleet timerboard for EVE Online "
+        }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         Router::<Route> {}
     }
